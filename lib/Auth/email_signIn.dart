153cec3d34a9e9079fc2b7045453a19edeba3ffa
Elly2018 <34325707+Elly2018@users.noreply.github.com>
2024-03-23 14:57:00 +0800

    update

--- lib/Auth/email_signIn.dart
+++ lib/Auth/email_signIn.dart
@@ -3,13 +3,14 @@ import 'package:flutter/foundation.dart';
 import 'package:mobile_client/Auth/auth.dart';
 import 'package:sprintf/sprintf.dart';
 
+const debug = true;
 
 class EmailSignInHelper {
   static String email = "";
   static String password = "";
 
   static SignInWithEmail() async {
-    if(kDebugMode){
+    if(debug){
       AuthState.Skip();
     }else{
       AuthState.SetCred(await FirebaseAuth.instance.signInWithEmailAndPassword(email: sprintf('%s@example.com', [email]), password: password));
